845b7340911eef5770263ef6ebfea66bcb4c83b8
Pouya <jeopard13@yahoo.com>
2024-05-13 09:18:48 +0330

    Starting contracts and node manager interface are  added

---
+++ src/interfaces/INodeManager.i.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+interface INodeManager {
+    // Function for registering a new node
+    function registerNode() external;
+
+    // Function for removing a registered node
+    function removeNode(address _nodeAddress) external;
+
+    // Function for updating the number of expeditionary forces allocated to a mission
+    function updateExpeditionaryForces(uint256 _expeditionaryForces) external;
+
+    // Function for updating the current geographical position of a node
+    function updateCurrentGeographicalPosition(
+        string memory _position
+    ) external;
+
+    // Function for updating the geographical position of the observed target by a node
+    function updateTargetGeographicalPosition(string memory _position) external;
+
+    // Function for updating the type of target declared by a node
+    function updateTargetType(string memory _type) external;
+
+    // View function to check if a given address is registered as a node
+    function isNodeRegistered(
+        address _nodeAddress
+    ) external view returns (bool);
+
+    // View function to retrieve the number of expeditionary forces allocated to a mission
+    function getNodeExpeditionaryForces(
+        address _nodeAddress
+    ) external view returns (uint256);
+
+    // View function to retrieve the current geographical position of a node
+    function getNodeCurrentGeographicalPosition(
+        address _nodeAddress
+    ) external view returns (string memory);
+
+    // View function to retrieve the geographical position of the observed target by a node
+    function getNodeTargetGeographicalPosition(
+        address _nodeAddress
+    ) external view returns (string memory);
+
+    // View function to retrieve the type of target declared by a node
+    function getNodeTargetType(
+        address _nodeAddress
+    ) external view returns (string memory);
+
+    // Events
+
+    // Event emitted when a new node is registered
+    event NodeRegistered(address indexed nodeAddress);
+
+    // Event emitted when a node is removed
+    event NodeRemoved(address indexed nodeAddress);
+
+    // Event emitted when the number of expeditionary forces allocated to a node is updated
+    event ExpeditionaryForcesUpdated(
+        address indexed nodeAddress,
+        uint256 newExpeditionaryForces
+    );
+
+    // Event emitted when the current geographical position of a node is updated
+    event CurrentGeographicalPositionUpdated(
+        address indexed nodeAddress,
+        string newPosition
+    );
+
+    // Event emitted when the geographical position of the observed target by a node is updated
+    event TargetGeographicalPositionUpdated(
+        address indexed nodeAddress,
+        string newTargetPosition
+    );
+
+    // Event emitted when the type of target declared by a node is updated
+    event TargetTypeUpdated(address indexed nodeAddress, string newTargetType);
+}
