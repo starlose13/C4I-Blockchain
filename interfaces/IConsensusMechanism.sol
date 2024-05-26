cb29cfbae6a1e2fb4d16d94d972eadd6402cd96b
Pouya <jeopard13@yahoo.com>
2024-05-26 16:17:41 +0330

    interfaces updated

--- interfaces/IConsensusMechanism.sol
+++ interfaces/IConsensusMechanism.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.18;
 import {DataTypes} from "../src/Helper/DataTypes.sol";
 
 interface IConsensusMechanism {
+    function reteriveTargetByAddress(
+        address _addr
+    ) external view returns (DataTypes.TargetLocation memory);
+
     // Function to initiate the consensus process
     function initiateConsensus() external;
 
@@ -26,4 +30,31 @@ interface IConsensusMechanism {
 
     // Event emitted when the consensus process is finalized
     event ConsensusFinalized();
+
+    function reportTargetLocation(
+        address _nodeAddress,
+        DataTypes.TargetZone _announceTarget
+    ) external;
+
+    function consensusAutomationExecution() external returns (bool isReached);
+
+    function checkConsensusReached() external view returns (uint256);
+
+    function hasNodeParticipated(
+        address _nodeAddress
+    ) external view returns (bool);
+
+    function deleteTargetLocation(uint index) external;
+
+    function resetToDefaults() external;
+
+    function resetAllTargetLocations() external;
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    ) external view returns (bool upkeepNeeded /* performData */);
+
+    function fetchConsensusThreshold() external view returns (uint64);
+
+    function performUpkeep(bytes calldata /* performData */) external;
 }
--- interfaces/INodeManager.sol
+++ interfaces/INodeManager.sol
@@ -3,87 +3,31 @@ pragma solidity 0.8.18;
 import {DataTypes} from "../src/Helper/DataTypes.sol";
 
 interface INodeManager {
-    // Function for registering a new node
-    // function registerNode(
-    //     address _nodeAddress,
-    //     string memory _currentPosition
-    // ) external;
+    function retrieveAllRegisteredNodeData()
+        external
+        view
+        returns (DataTypes.RegisteredNodes[] memory);
 
-    // // Function for removing a registered node
-    // function unRegisterNode(address _nodeAddress) external;
+    function registerNewNode(
+        address _nodeAddress,
+        DataTypes.NodeRegion _currentPosition,
+        string memory IPFS
+    ) external;
 
-    // // Function for updating the number of expeditionary forces allocated to a mission
-    // function updateExpeditionaryForces(uint256 _expeditionaryForces) external;
+    function updateExpeditionaryForces(
+        DataTypes.NodeRegion expeditionaryForces,
+        address _nodeAddress
+    ) external;
 
-    // // Function for updating the current geographical position of a node
-    // function updateCurrentGeographicalPosition(
-    //     string memory _position
-    // ) external;
-
-    // // Function for updating the geographical position of the observed target by a node
-    // function updateTargetGeographicalPosition(string memory _position) external;
-
-    // // Function for updating the type of target declared by a node
-    // function updateTargetType(string memory _type) external;
-
-    // // View function to retrieve the number of expeditionary forces allocated to a mission
-    // function getNodeExpeditionaryForces(
-    //     address _nodeAddress
-    // ) external view returns (uint256);
-
-    // // View function to retrieve the current geographical position of a node
-    // function getNodeCurrentGeographicalPosition(
-    //     address _nodeAddress
-    // ) external view returns (string memory);
-
-    // // View function to retrieve the geographical position of the observed target by a node
-    // function getNodeTargetGeographicalPosition(
-    //     address _nodeAddress
-    // ) external view returns (string memory);
-
-    // // View function to retrieve the type of target declared by a node
-    // function getNodeTargetType(
-    //     address _nodeAddress
-    // ) external view returns (string memory);
-
-    // View function to check if a given address is registered as a node
-
-    function isNodeRegistered(address nodeAddress) external view returns (bool);
-
-    function numberOfPresentNodes() external view returns (uint);
+    function numberOfPresentNodes() external view returns (uint count);
 
     function retrieveAddressByIndex(uint index) external view returns (address);
+
+    function isNodeRegistered(address nodeAddress) external view returns (bool);
 
     // // Events
     event NodeRegistered(
         address indexed nodeAddress,
         DataTypes.NodeRegion currentPosition
     );
-
-    // // Event emitted when a new node is registered
-    // event NodeRegistered(address indexed nodeAddress);
-
-    // // Event emitted when a node is removed
-    // event NodeRemoved(address indexed nodeAddress);
-
-    // // Event emitted when the number of expeditionary forces allocated to a node is updated
-    // event ExpeditionaryForcesUpdated(
-    //     address indexed nodeAddress,
-    //     uint256 newExpeditionaryForces
-    // );
-
-    // // Event emitted when the current geographical position of a node is updated
-    // event CurrentGeographicalPositionUpdated(
-    //     address indexed nodeAddress,
-    //     string newPosition
-    // );
-
-    // // Event emitted when the geographical position of the observed target by a node is updated
-    // event TargetGeographicalPositionUpdated(
-    //     address indexed nodeAddress,
-    //     string newTargetPosition
-    // );
-
-    // // Event emitted when the type of target declared by a node is updated
-    // event TargetTypeUpdated(address indexed nodeAddress, string newTargetType);
 }
