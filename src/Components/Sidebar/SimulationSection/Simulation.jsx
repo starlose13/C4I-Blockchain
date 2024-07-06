0c38fb4e0987784597d52aace3f7bae8b88d0c7f
parisasarraf <Parisasarraf79@gmail.com>
2024-07-06 15:48:46 +0330

    feat(address-card): open modal on click

--- src/Components/Sidebar/SimulationSection/Simulation.jsx
+++ src/Components/Sidebar/SimulationSection/Simulation.jsx
@@ -1,44 +1,168 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddressCard from './AddressCard';
 import useAddressData from '../../../hooks/useAddressData';
-import { ModalSimulation } from './ModalSimolation';
 import './scrollbar.css';
 
 
-/**
- * Simulation Component
- * @description This component fetches address data and renders the simulation UI.
- * @returns {JSX.Element} The rendered component
- */
 const Simulation = () => {
-    // Custom hook to fetch address data
-    const { data, loading, error } = useAddressData();
+  const { data, loading, error } = useAddressData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // Handle loading state
-    if (loading) return <div className="text-white">Loading...</div>;
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-    // Handle error state
-    if (error) return <div className="text-red-500">Error: {error.message}</div>;
+  if (loading) return <div className="text-white">Loading...</div>;
+  if (error) return <div className="text-red-500">Error: {error.message}</div>;
 
-    // Main render
-    return (
-        <div>
-            <div className="w-[40rem] p-4 custom-scrollbar">
-                <h4 className="text-white mb-4 font-bold">Simulation</h4>
-                
-                    <div
-                        className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 h-64 w-[40rem] overflow-y-scroll"
-                    >
-                        {data.map((addressData, index) => (
-                            <AddressCard key={index} addressData={addressData} />
-                        ))}
-                    </div>
-                <button className="w-full text-sm bg-[#298bfe] text-[#d7e6f7] h-10">
-                    Run Simulation
-                </button>
-            </div>
+
+
+  return (
+    <div>
+      <div className="w-[40rem] p-4 custom-scrollbar">
+        <h4 className="text-white mb-4 font-bold">Simulation</h4>
+        <div onClick={toggleModal} className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 h-72 w-[40rem] overflow-y-scroll">
+          {data.map((addressData, id) => (
+            <AddressCard key={id} addressData={addressData} />
+          ))}
         </div>
-    );
+        <button
+          className="w-full text-sm bg-[#298bfe] text-[#d7e6f7] h-10"
+        // onClick={toggleModal}
+        >
+          Run Simulation
+        </button>
+      </div>
+      {isModalOpen && (
+        <div
+          tabIndex="-1"
+          aria-hidden="true"
+          className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
+        >
+          <div className="relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+            {/* Modal content */}
+            <div className="relative">
+              {/* Modal header */}
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  Create New Product
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  onClick={toggleModal}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              {/* Modal body */}
+              <form className="p-4 md:p-5">
+                <div className="grid gap-4 mb-4 grid-cols-2">
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Type product name"
+                      required
+                    />
+                  </div>
+                  <div className="col-span-2 sm:col-span-1">
+                    <label
+                      htmlFor="price"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      name="price"
+                      id="price"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="$2999"
+                      required
+                    />
+                  </div>
+                  <div className="col-span-2 sm:col-span-1">
+                    <label
+                      htmlFor="category"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Category
+                    </label>
+                    <select
+                      id="category"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    >
+                      <option selected>Select category</option>
+                      <option value="TV">TV/Monitors</option>
+                      <option value="PC">PC</option>
+                      <option value="GA">Gaming/Console</option>
+                      <option value="PH">Phones</option>
+                    </select>
+                  </div>
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="description"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Product Description
+                    </label>
+                    <textarea
+                      id="description"
+                      rows="4"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Write product description here"
+                    ></textarea>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  <svg
+                    className="me-1 -ms-1 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Add new product
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Simulation;
