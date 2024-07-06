f3cc569a1aef6967c65da66eec7cb0138636e9fe
parisasarraf <Parisasarraf79@gmail.com>
2024-07-06 17:18:31 +0330

    style(modal): update modal appearance and layout

--- src/Components/Sidebar/SimulationSection/Simulation.jsx
+++ src/Components/Sidebar/SimulationSection/Simulation.jsx
@@ -28,7 +28,6 @@ const Simulation = () => {
         </div>
         <button
           className="w-full text-sm bg-[#298bfe] text-[#d7e6f7] h-10"
-        // onClick={toggleModal}
         >
           Run Simulation
         </button>
@@ -37,125 +36,100 @@ const Simulation = () => {
         <div
           tabIndex="-1"
           aria-hidden="true"
-          className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
+          className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-[rgb(41, 139, 254)] backdrop-blur-[20px]"
         >
-          <div className="relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="relative p-4 w-full max-w-md max-h-full bg-[rgba(12, 74, 110, 0.47)]">
             {/* Modal content */}
             <div className="relative">
               {/* Modal header */}
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  Create New Product
-                </h3>
-                <button
-                  type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                  onClick={toggleModal}
-                >
-                  <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                    />
-                  </svg>
-                  <span className="sr-only">Close modal</span>
-                </button>
+              <div className="flex items-center justify-between p-4 md:p-5 ">
+                <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Add New Location</h3>
+
               </div>
               {/* Modal body */}
               <form className="p-4 md:p-5">
                 <div className="grid gap-4 mb-4 grid-cols-2">
+
                   <div className="col-span-2">
                     <label
                       htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
                     >
-                      Name
+                      Position
                     </label>
                     <input
                       type="text"
                       name="name"
                       id="name"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="Type product name"
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:outline-none"
+                      placeholder="Eiffel Tower, Paris, France"
                       required
                     />
                   </div>
-                  <div className="col-span-2 sm:col-span-1">
+
+
+                  <div className="col-span-1">
                     <label
-                      htmlFor="price"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
                     >
-                      Price
+                      Latitude
                     </label>
                     <input
-                      type="number"
-                      name="price"
-                      id="price"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="$2999"
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
+                      placeholder="48.8584° N"
                       required
                     />
                   </div>
-                  <div className="col-span-2 sm:col-span-1">
+
+                  <div className="col-span-1">
                     <label
-                      htmlFor="category"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
                     >
-                      Category
+                      Longitude
                     </label>
-                    <select
-                      id="category"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    >
-                      <option selected>Select category</option>
-                      <option value="TV">TV/Monitors</option>
-                      <option value="PC">PC</option>
-                      <option value="GA">Gaming/Console</option>
-                      <option value="PH">Phones</option>
-                    </select>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
+
+
+                      placeholder="2.2945° E"
+                      required
+                    />
                   </div>
-                  <div className="col-span-2">
-                    <label
-                      htmlFor="description"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Product Description
-                    </label>
-                    <textarea
-                      id="description"
-                      rows="4"
-                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Write product description here"
-                    ></textarea>
-                  </div>
+
                 </div>
-                <button
-                  type="submit"
-                  className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  <svg
-                    className="me-1 -ms-1 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
+
+                <div className='flex justify-end mt-14'>
+                  <button
+                    type="submit"
+                    className="mr-4 text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  Add new product
-                </button>
+
+                    Submit
+                  </button>
+
+                  <button
+                    type="button"
+                    className=" text-white inline-flex items-center bg-[#d81d1d] hover:bg-[#a61919] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    onClick={toggleModal}
+                  >
+                    Close
+                  </button>
+
+
+
+                </div>
+
+
+
+
               </form>
             </div>
           </div>
