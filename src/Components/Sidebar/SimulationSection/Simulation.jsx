c9c33e1795c6704f480c119b6d31d6ec73c3ffbf
parisasarraf <Parisasarraf79@gmail.com>
2024-07-07 09:24:59 +0330

    feat(simulation): adjust width and add modal toggle

--- src/Components/Sidebar/SimulationSection/Simulation.jsx
+++ src/Components/Sidebar/SimulationSection/Simulation.jsx
@@ -1,34 +1,38 @@
-import React, { useState } from 'react';
-import AddressCard from './AddressCard';
-import useAddressData from '../../../hooks/useAddressData';
-import './scrollbar.css';
+import React, { useState } from "react";
+import AddressCard from "./AddressCard";
+import useAddressData from "../../../hooks/useAddressData";
+import "./scrollbar.css";
+
+
 
 
 const Simulation = () => {
   const { data, loading, error } = useAddressData();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
+
+
   if (loading) return <div className="text-white">Loading...</div>;
   if (error) return <div className="text-red-500">Error: {error.message}</div>;
-
-
 
   return (
     <div>
       <div className="w-[40rem] p-4 custom-scrollbar">
         <h4 className="text-white mb-4 font-bold">Simulation</h4>
-        <div onClick={toggleModal} className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 h-72 w-[40rem] overflow-y-scroll">
+        <div
+          onClick={toggleModal}
+          className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 h-72 w-[38rem] overflow-y-scroll"
+        >
           {data.map((addressData, id) => (
             <AddressCard key={id} addressData={addressData} />
           ))}
         </div>
-        <button
-          className="w-full text-sm bg-[#298bfe] text-[#d7e6f7] h-10"
-        >
+        <button className="w-full text-sm bg-[#298bfe] text-[#d7e6f7] h-10">
           Run Simulation
         </button>
       </div>
@@ -38,22 +42,19 @@ const Simulation = () => {
           aria-hidden="true"
           className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-[rgb(41, 139, 254)] backdrop-blur-[20px]"
         >
-          <div className="relative p-4 w-full max-w-md max-h-full bg-[rgba(12, 74, 110, 0.47)]">
-            {/* Modal content */}
+          <div className="relative p-4 w-full max-w-md max-h-full bg-[#0a324670] rounded-lg">
             <div className="relative">
-              {/* Modal header */}
               <div className="flex items-center justify-between p-4 md:p-5 ">
-                <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">Add New Location</h3>
-
+                <h3 className="text-2xl font-semibold text-gray-900 dark:text-white">
+                  Add New Location
+                </h3>
               </div>
-              {/* Modal body */}
               <form className="p-4 md:p-5">
                 <div className="grid gap-4 mb-4 grid-cols-2">
-
                   <div className="col-span-2">
                     <label
                       htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
+                      className="block mb-2 text-sm font-medium text-slate-400"
                     >
                       Position
                     </label>
@@ -61,17 +62,16 @@ const Simulation = () => {
                       type="text"
                       name="name"
                       id="name"
-                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:outline-none"
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5  placeholder-slate-400 text-red focus:outline-none"
                       placeholder="Eiffel Tower, Paris, France"
                       required
                     />
                   </div>
 
-
                   <div className="col-span-1">
                     <label
                       htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
+                      className="block mb-2 text-sm font-medium text-slate-400"
                     >
                       Latitude
                     </label>
@@ -79,7 +79,7 @@ const Simulation = () => {
                       type="text"
                       name="name"
                       id="name"
-                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
                       placeholder="48.8584° N"
                       required
                     />
@@ -88,7 +88,7 @@ const Simulation = () => {
                   <div className="col-span-1">
                     <label
                       htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 text-slate-400"
+                      className="block mb-2 text-sm font-medium text-slate-400"
                     >
                       Longitude
                     </label>
@@ -96,22 +96,18 @@ const Simulation = () => {
                       type="text"
                       name="name"
                       id="name"
-                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
-
-
+                      className="bg-sky-950 border border-sky-900 text-gray-100 text-sm rounded-lg block w-full p-2.5 placeholder-slate-400 text-red focus:ring-sky-950 focus:border-sky-950 focus:outline-none"
                       placeholder="2.2945° E"
                       required
                     />
                   </div>
+                  </div>
 
-                </div>
-
-                <div className='flex justify-end mt-14'>
+                <div className="flex justify-end mt-14">
                   <button
                     type="submit"
                     className="mr-4 text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
-
                     Submit
                   </button>
 
@@ -122,14 +118,7 @@ const Simulation = () => {
                   >
                     Close
                   </button>
-
-
-
                 </div>
-
-
-
-
               </form>
             </div>
           </div>
