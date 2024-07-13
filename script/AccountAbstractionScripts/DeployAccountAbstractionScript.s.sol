e8e19a5b9bd7941f05267e09938bccde3355d4ea
Pouya <jeopard13@yahoo.com>
2024-07-13 15:03:06 +0330

    finalize Script.sol of Account Abstraction

--- script/AccountAbstractionScripts/DeployAccountAbstractionScript.s.sol
+++ script/AccountAbstractionScripts/DeployAccountAbstractionScript.s.sol
@@ -3,18 +3,20 @@ pragma solidity 0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {MinimalAccountAbstraction} from "../../contracts/ethereum/MinimalAccountAbstraction.sol";
-import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {HelperConfig} from "script/AccountAbstractionScripts/HelperConfig.s.sol";
 
 contract AccountAbstractionScript is Script {
     MinimalAccountAbstraction private minimalAccountAbstraction;
-    IEntryPoint private entryPoint;
+    HelperConfig private helperConfig;
     address userAddress = makeAddr("minimalAccount");
 
     function run() external returns (MinimalAccountAbstraction) {
         vm.startBroadcast();
         vm.prank(userAddress);
+        helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
         minimalAccountAbstraction = new MinimalAccountAbstraction(
-            address(entryPoint)
+            config.entryPoint
         );
         vm.stopBroadcast();
         return minimalAccountAbstraction;
--- script/AccountAbstractionScripts/HelperConfig.s.sol
+++ script/AccountAbstractionScripts/HelperConfig.s.sol
@@ -14,8 +14,9 @@ contract HelperConfig is Script {
     uint256 private constant ETH_SEPOLIA_TESTNET_CHAIN_ID = 11155111;
     uint256 private constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 private constant LOCAL_CHAIN_ID = 31337;
-    uint256 private constant ARBITRUM_MAINNET_CHAIN_ID = 42_161;
     uint256 private constant ZKSYNC_CHAIN_ID = 324;
+    uint256 private constant ARBITRUM_MAINNET_CHAIN_ID = 42_161;
+
     address constant BURNER_WALLET = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address constant ANVIL_DEFAULT_ACCOUNT =
         0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
@@ -28,6 +29,11 @@ contract HelperConfig is Script {
         networkConfigs[ETH_MAINNET_CHAIN_ID] = getETHMainNetConfig();
         networkConfigs[ETH_SEPOLIA_TESTNET_CHAIN_ID] = getETHSepoliaConfig();
         networkConfigs[ZKSYNC_CHAIN_ID] = getZkSyncConfig();
+        networkConfigs[ARBITRUM_MAINNET_CHAIN_ID] = getARbitrumMainNetConfig();
+    }
+
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
     }
 
     function getConfigByChainId(
@@ -62,6 +68,18 @@ contract HelperConfig is Script {
         return NetworkConfig({entryPoint: address(0), account: BURNER_WALLET});
     }
 
+    function getARbitrumMainNetConfig()
+        public
+        pure
+        returns (NetworkConfig memory)
+    {
+        return
+            NetworkConfig({
+                entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+                account: BURNER_WALLET
+            });
+    }
+
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
@@ -70,7 +88,6 @@ contract HelperConfig is Script {
         console.log("Deploying mocks...");
         vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
         EntryPoint entryPoint = new EntryPoint();
-        // ERC20Mock erc20Mock = new ERC20Mock();
         vm.stopBroadcast();
         console.log("Mocks deployed!");
 
