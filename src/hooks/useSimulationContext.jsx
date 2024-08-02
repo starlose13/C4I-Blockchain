74c6999f4af46f86164e953cd1329befc8a8bbb4
ParisaSarraf <132214177+ParisaSarraf@users.noreply.github.com>
2024-08-02 12:47:47 +0330

    refactor(simulation): remove example code from simulation context

--- src/hooks/useSimulationContext.jsx
+++ src/hooks/useSimulationContext.jsx
@@ -1,44 +1,4 @@
-// import React, { createContext, useState, useEffect } from "react";
-// import dataArea from '../../src/Components/MapComponent/areas.json';
-// import useAddressData from './useAddressData';
-//
-// export const SimulationData = createContext();
-//
-// export const SimulationDataProvider = ({ children }) => {
-//     const { data: addressData, loading, error } = useAddressData();
-//     const [data, setData] = useState({
-//         location: "",
-//         latitude: "",
-//         longitude: "",
-//     });
-//
-//     const updateData = (areaId) => {
-//         const area = dataArea.find(a => a.name === areaId);
-//         if (!area) {
-//             console.warn("Area not found for areaId:", areaId);
-//             return;
-//         }
-//
-//         const addressInfo = addressData.find(a => a.address === area.publicAddress);
-//         if (!addressInfo) {
-//             console.warn("Address info not found for publicAddress:", area.publicAddress);
-//             return;
-//         }
-//
-//         setData({
-//             location: addressInfo.location || "",
-//             latitude: addressInfo.latitude || "",
-//             longitude: addressInfo.longitude || "",
-//         });
-//     };
-//
-//     return (
-//         <SimulationData.Provider value={{ data, updateData, loading, error }}>
-//             {children}
-//         </SimulationData.Provider>
-//     );
-// };
-
 import {createContext} from "react";
+
 export const MainContext = createContext()
 
