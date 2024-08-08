b4b5e8464acabffe18ac35b3e6861354ae097a46
parisasarraf <Parisasarraf79@gmail.com>
2024-08-08 09:13:38 +0330

    style(useGetContract):add professional comment

--- src/hooks/useGetContract.jsx
+++ src/hooks/useGetContract.jsx
@@ -1,28 +1,37 @@
+/*//////////////////////////////////////////////////////////////
+                               IMPORTS
+    //////////////////////////////////////////////////////////////*/
 import { ethers } from 'ethers';
 import { useEffect, useState } from "react";
 import NodeManagerABI from "../utils/NodeManagerABI.json"
 import ConsensusMechanismABI from "../utils/ConsensusMechanismABI.json"
 
-// Load environment variables
+/*//////////////////////////////////////////////////////////////
+                      LOAD ENVIROMENT VARIABLES
+    //////////////////////////////////////////////////////////////*/
 const anvilUrl = import.meta.env.VITE_RPC_URL;
 const privateKey = import.meta.env.VITE_PRIVATE_KEY;
 const NodeManagerContractAddress = import.meta.env.VITE_NODEMANAGER_CONTRACT_ADDRESS;
 const ConsensusMechanismContractAddress = import.meta.env.VITE_CONSENSUS_CONTRACT_ADDRESS;
 
-// Initialize the provider and wallet
+
+/*//////////////////////////////////////////////////////////////
+                  INITIALIZE THE PROVIDER AND WALLET
+    //////////////////////////////////////////////////////////////*/
 const provider = new ethers.JsonRpcProvider(anvilUrl);
 const wallet = new ethers.Wallet(privateKey, provider);
 
-// console.log('ABI:', NodeManagerABI);
-// console.log('ABI:', ConsensusMechanismABI);
 
-// Create a contract instance
+/*//////////////////////////////////////////////////////////////
+                      CREATE A CONTRACT INSTANCE
+    //////////////////////////////////////////////////////////////*/
 const NodeManagerContract = new ethers.Contract(NodeManagerContractAddress, NodeManagerABI, wallet);
 const ConsensusMechanismContract = new ethers.Contract(ConsensusMechanismContractAddress, ConsensusMechanismABI, wallet);
 
-const s_agents = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"];
-const s_announceTargets = ["1"];
 
+/*//////////////////////////////////////////////////////////////
+                        NODE MANAGER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 export const useInteractWithNodeManagerContract = () => {
 
     const [result, setResult] = useState()
@@ -32,8 +41,8 @@ export const useInteractWithNodeManagerContract = () => {
             try {
                 // const data = await NodeManagerContract.retrieveAllRegisteredNodeData();
                 const data = await NodeManagerContract.getNodeAddresses();
-                console.log("getnodeAddresses is: ",data);
-                
+                console.log("getnodeAddresses is: ", data);
+
                 // const structuredData = data.map((nodeData, index) => ({
                 //     // id: index,
                 //     // address: nodeData.nodeAddress,
@@ -54,10 +63,15 @@ export const useInteractWithNodeManagerContract = () => {
     return { result, error };
 }
 
+
+
+/*//////////////////////////////////////////////////////////////
+                          CONSENSUS FUNCTION
+    //////////////////////////////////////////////////////////////*/
 export const useInteractWithConsensusContractOnChainData = () => {
 
     const [error, setError] = useState(null);
-    
+
     useEffect(() => {
 
         const fetchData = async () => {
@@ -65,17 +79,17 @@ export const useInteractWithConsensusContractOnChainData = () => {
             try {
 
                 const data = await NodeManagerContract.URIDataFormatter('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
-                const _data = await ConsensusMechanismContract.TargetLocationSimulation(s_agents,s_announceTargets);
+                const _data = await ConsensusMechanismContract.TargetLocationSimulation(s_agents, s_announceTargets);
 
                 // console.log(data);
-                
+
                 console.log('Transaction sent:', data);
                 console.log('Transaction sent:', _data);
 
             } catch (err) {
                 console.error('Error interacting with the contract:', err);
                 setError(err);
-            
+
             }
         };
         fetchData();
