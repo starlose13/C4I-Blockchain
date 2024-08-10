45cf5af0ba052d2a466e09b71d6273c63dee4153
Pouya <jeopard13@yahoo.com>
2024-08-10 15:03:20 +0330

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sphinx_rtd_theme
 project = 'decentralized communication'
 copyright = '2024, Pouya.N'
 author = 'Pouya.N'
@@ -14,16 +13,10 @@ release = 'v0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinxcontrib.solidity',]
-html_theme = 'sphinx_rtd_theme'
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-solidity_options = {
-    'source': 'path/to/your/solidity/files',
-    'output': 'docs/_build/solidity',
-}
+
 
 
 
