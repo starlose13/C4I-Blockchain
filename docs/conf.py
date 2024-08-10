66f9f7e0cbcf82436ba0b6fa5a2ba27c52cc4db3
Pouya <jeopard13@yahoo.com>
2024-08-10 11:48:41 +0330

    add first document

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,20 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# conf.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Project information
+project = 'Read the Docs Tutorial'
+copyright = '2024, Your Name'
+author = 'Your Name'
+release = '0.1'
 
-project = 'Blockchain SunAir'
-copyright = '2024, Pouya'
-author = 'Pouya'
-release = 'v0.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output
 html_theme = 'alabaster'
 html_static_path = ['_static']
