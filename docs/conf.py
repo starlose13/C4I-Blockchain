eb4b095fca6d6120b4628598a04403f692acd9e0
Pouya <jeopard13@yahoo.com>
2024-08-10 14:30:45 +0330

    Add table of contents and section files

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,20 @@
+# conf.py
+import os
+import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 # Project information
-project = 'Read the Docs Tutorial'
 project = 'Your Project Name'
 copyright = '2024, Your Name'
 author = 'Your Name'
 release = '0.1'
+
+# General configuration
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# HTML output
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 # conf.py
-
+import os
+import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 # Project information
