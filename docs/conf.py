bbd0789c81114ba7bb3442a35fa1d13c225304b5
Pouya <jeopard13@yahoo.com>
2024-08-11 09:51:19 +0330

    change to html_theme = 'sphinx_rtd_theme'  # Switch to Read the Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -23,5 +23,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  # Switch to Read the Docs theme
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
