c38cc26c67e1757ab4dcb0bf130244b8da933321
Pouya <jeopard13@yahoo.com>
2024-08-14 13:36:29 +0330

    update meta data of conf.py project

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to decentralized communication's documentation!
+Welcome to Decentralized Communication's Documentation!
 =======================================================
 .. toctree::
    :maxdepth: 5
--- docs/conf.py
+++ docs/conf.py
@@ -5,10 +5,61 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-project = 'decentralized communication'
-copyright = '2024, Pouya.N'
+
+project = 'Decentralized Communication Framework'
+"""The name of the project or documentation. Ensure this is a clear and descriptive name
+for the documentation set, reflecting the scope and objectives of the project."""
+
+copyright = '2024, SunAir Institute'
+"""The copyright statement for the documentation. This typically includes the year and
+the name of the organization or individual holding the copyright."""
+
 author = 'Pouya.N'
-release = 'v0.1'
+"""The primary author or contributor of the documentation. This field should be filled
+with the name of the person or team responsible for creating the content."""
+
+release = 'v1.0'
+"""The release version of the documentation. This should correspond to the version of
+the software or project being documented, and it is useful for versioning and tracking
+changes over time."""
+
+version = '1.0'
+"""The version of the project or software. This should match the major version of the
+release and helps users identify the documentation's relevance to specific versions of
+the project."""
+
+# Optional metadata that can be used for further project identification and management
+html_title = 'Decentralized Communication Framework Documentation'
+"""The title displayed in the HTML output of the documentation. It's often used in
+browser titles and other navigational elements."""
+
+html_short_title = 'Decentralized Communication'
+"""A shorter version of the title, used for breadcrumb trails and other space-limited
+areas in the documentation."""
+
+html_last_updated_fmt = '%b %d, %Y'
+"""Format for displaying the last updated date in the documentation, ensuring users are
+aware of the currency of the content."""
+
+project_url = 'https://github.com/SunAirInstitute/DecentralizedCommunication'
+"""The URL to the project's repository or main website. This provides users with a direct
+link to access the project source code or further information."""
+
+# Example of a complete project metadata setup
+project_metadata = {
+    'name': project,
+    'version': version,
+    'release': release,
+    'copyright': copyright,
+    'author': author,
+    'title': html_title,
+    'short_title': html_short_title,
+    'last_updated': html_last_updated_fmt,
+    'project_url': project_url,
+}
+"""A dictionary containing key metadata about the project, which can be used throughout
+the documentation for consistency and reference."""
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,7 +67,6 @@ release = 'v0.1'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 
@@ -30,3 +80,4 @@ html_static_path = ['_static']
 html_css_files = [
     'custom.css',
 ]
+
