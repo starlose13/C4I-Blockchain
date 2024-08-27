d13538e29d2977384b92f40954504530b9863ed3
parisasarraf <Parisasarraf79@gmail.com>
2024-08-27 08:35:44 +0330

    fea:add uint name for show in card

--- src/Components/MapComponent/NodeTooltip.jsx
+++ src/Components/MapComponent/NodeTooltip.jsx
@@ -1,47 +1,44 @@
-const NodeTooltip = ({ address,area, nodeLat,nodeLan,visible, position, radius }) => {
+const NodeTooltip = ({ address, area, nodeLat, nodeLan, visible, position, radius, unitName }) => {
   if (!visible) return null;
 
- 
+
   return (
     <div
-      className="absolute bg-[rgba(41, 139, 254)] backdrop-blur-[10px] border-t-8 border-[#298BFE] w-[17rem]"
+      className="absolute bg-[rgba(41, 139, 254)] backdrop-blur-[10px] border-t-8 border-[#298BFE] w-[19rem]"
       style={{ top: position.y, left: position.x }}
     >
-      {/* {mappedData.map((data, index) => ( */}
-        {/* nodeDataMap.name === data.walletAddress && ( */}
-          <div  className="node-item">
-            <div className="bg-[#1f426f] pb-2">
-              <div className="text-[#298BFE] text-xl font-bold p-3">
+      <div className="node-item">
+        <div className="bg-[#1f426f] pb-2">
+          <div className="text-[#298BFE] text-3xl font-bold p-3">
+            {unitName}
+          </div>
+        </div>
+        <div>
+          <div className="text-gray-300 p-3">
+            <div>
+              <div className="text-sm font-bold">Address</div>
+              <div className="text-[10px] text-[#5a6b6d]">
+                {address}
               </div>
             </div>
-            <div>
-              <div className="text-gray-300 p-3">
-                <div>
-                  <div className="text-sm font-bold">Address</div>
-                  <div className="text-[10px] text-[#5a6b6d]">
-                  {address}
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <div className="text-sm font-bold">Position</div>
-                  <div className="text-xs font-bold text-[#5a6b6d]">
-                    Location: {area}
-                  </div>
-                  <div className="text-xs font-bold text-[#5a6b6d]">
-                    Latitude:  {nodeLat}
-                  </div>
-                  <div className="text-xs font-bold text-[#5a6b6d]">
-                    Longitude: {nodeLan}
-                  </div>
-                </div>
+            <div className="mt-2">
+              <div className="text-sm font-bold">Position</div>
+              <div className="text-xs font-bold text-[#5a6b6d]">
+                Location: {area}
+              </div>
+              <div className="text-xs font-bold text-[#5a6b6d]">
+                Latitude:  {nodeLat}
+              </div>
+              <div className="text-xs font-bold text-[#5a6b6d]">
+                Longitude: {nodeLan}
               </div>
             </div>
           </div>
-        {/* ) */}
-      {/* ))} */}
+        </div>
+      </div>
     </div>
   );
-  
+
 
 
 
