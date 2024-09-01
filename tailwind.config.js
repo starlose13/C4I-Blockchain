b149f105dd0b3eafd0b076c63cef5a2905c96dc8
parisasarraf <Parisasarraf79@gmail.com>
2024-09-01 11:54:11 +0330

    feat : change background spinner blur

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        screens: {
+            'sm': '576px',
+            // => @media (min-width: 576px) 
+            'md': '960px',
+            // => @media (min-width: 960px) 
+            'lg': '1440px',
+            // => @media (min-width: 1440px) 
+        },
         extend: {
             boxShadow: {
                 'custom-raise': '0 0.5em 0.5em -0.4em var(--hover)',
